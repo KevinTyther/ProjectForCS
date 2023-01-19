67dccad4e4200f3a7657edf0ca34e96e27b1f957
Kevin Tyther <kevin.tyther@vtsd.com>
2023-01-19 12:27:42 -0500

    Pusin

--- ProjectCSP.cpp
+++ ProjectCSP.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 void Login();
-void askForSecretPassword();
 
 bool login = true;
 string usrname;
@@ -24,39 +23,34 @@ void Login()
         system("cls");
         cout << "Username: ";
         cin >> usrname;
-        askForSecretPassword();
-    }
-}
-
-void askForSecretPassword(void) 
-{
-    passwd = "";
-    while (!GetAsyncKeyState(VK_RETURN) & 1)
-    {
-        for (int i = 0x30; i < 0x5A; i++)
+        cout << "Password: ";
+        while (!GetAsyncKeyState(VK_RETURN) & 1)
         {
-            if (GetAsyncKeyState(i) & 1)
+            for (int i = 0x30; i < 0x5A; i++)
             {
-                if (i >= 0x41 && i <= 0x5A && ((GetKeyState(VK_CAPITAL) & 1) == 0 || GetAsyncKeyState(VK_SHIFT) & 1))
-                    passwd += ((char)i + 32);
-                else
-                    passwd += (char)i;
-
-                cout << "*";
-                Sleep(50);
-            }
-            else if (GetAsyncKeyState(VK_BACK) & 1)
-            {
-                passwd.erase(passwd.size() - 1);
-                system("cls");
-                for (int i = 0; i < passwd.size(); i++)
+                if (GetAsyncKeyState(i) & 1)
                 {
-                    cout << '*';
+                    if (i >= 0x41 && i <= 0x5A && ((GetKeyState(VK_CAPITAL) & 1) == 0 || GetAsyncKeyState(VK_SHIFT) & 1))
+                        passwd += ((char)i + 32);
+                    else
+                        passwd += (char)i;
+
+                    cout << "*";
+                    Sleep(50);
                 }
-                Sleep(50);
+                else if (GetAsyncKeyState(VK_BACK) & 1)
+                {
+                    passwd.erase(passwd.size() - 1);
+                    system("cls");
+                    for (int i = 0; i < passwd.size(); i++)
+                    {
+                        cout << '*';
+                    }
+                    Sleep(50);
+                }
             }
         }
+        cout << passwd;
+        Sleep(10000);
     }
-    cout << passwd;
-    Sleep(10000);
-};
+}
