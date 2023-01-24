6728fe2e3379d130f166c6b5409d113c4c2eaeda
Kevin Tyther <90258258+KevinTyther@users.noreply.github.com>
2023-01-24 06:30:22 -0500

    Update ProjectCSP.cpp
    transaction log time

--- ProjectCSP.cpp
+++ ProjectCSP.cpp
@@ -11,19 +11,34 @@ string usrname;
 string usrChecker;
 
 char c;
+int i = 0;
+int m = 0;
+int counter;
 
 int pin;
 int pinChecker;
 int choice;
-int balance = 0;
-int deposit;
-int withdraw;
+int lastTrans;
+int test = 1;
+int test2 = 1;
+float balance = 0;
+float deposit;
+float withdraw;
+float lastAction;
+string moneyMovement;
+
+string MM[6];
+float transaction[6];
 
 bool loginScreen;
 bool bankMenu = true;
 bool validator = true;
+bool pinLock;
+bool transactionMenu;
 
 void usrVaildator();
+void PinValidator();
+void TransactionLog();
 void Password();
 void Username();
 void Pincode();
@@ -69,19 +84,56 @@ void BankMenu()
 {
 	while (BankMenu)
 	{
+		pinLock = false;
 		system("cls");
 		cout << "Welcome " << usrChecker << "\n";
 		cout << "Balance: $" << balance << "\n\n";
 		cout << "1. Deposit\n";
-		cout << "2. Withdraw\n\n";
+		cout << "2. Withdraw\n";
+		cout << "3. Transaction Log\n\n";
 		cin >> choice;
 		if (choice == 1)
 		{
+			pinLock = true;
+			system("cls");
+			while (pinLock)
+			{
+				PinValidator();
+			}
 			system("cls");
 			cout << "How much would you like to deposit\n\n";
 			cout << "Amount: ";
 			cin >> deposit;
-			balance = deposit;
+			balance = balance + deposit;
+			lastAction = deposit;
+			moneyMovement = " deposit ";
+			counter = counter + 1;
+			for (i; i <= test; i++)
+			{
+				transaction[i] = lastAction;
+			}
+			if (i > 5)
+			{
+				i = 0;
+			}
+			test = test + 1;
+			if (test > 5)
+			{
+				test = 1;
+			}
+			for (m; m <= test; m++)
+			{
+				MM[m] = moneyMovement;
+			}
+			if (m > 5)
+			{
+				m = 0;
+			}
+			test2 = test2 + 1;
+			if (test2 > 5)
+			{
+				test2 = 1;
+			}
 			Sleep(500);
 			system("cls");
 			cout << "Deposit was successful\n";
@@ -89,6 +141,12 @@ void BankMenu()
 		}
 		if (choice == 2)
 		{
+			system("cls");
+			pinLock = true;
+			while (pinLock)
+			{
+				PinValidator();
+			}
 			system("cls");
 			cout << "How much money would you like to withdraw\n\n";
 			cout << "Amount: ";
@@ -101,8 +159,101 @@ void BankMenu()
 			else
 			{
 				balance = balance - withdraw;
+				lastAction = withdraw;
+				moneyMovement = " withdrawal ";
+				counter = counter + 1;
+				for (i; i <= test; i++)
+				{
+					transaction[i] = lastAction;
+				}
+				if (i > 5)
+				{
+					i = 0;
+				}
+				test = test + 1;
+				if (test > 5)
+				{
+					test = 1;
+				}
+				for (int m = 0; m <= test; m++)
+				{
+					MM[m] = moneyMovement;
+				}
+				if (m > 5)
+				{
+					m = 0;
+				}
+				test2 = test2 + 1;
+				if (test2 > 5)
+				{
+					test2 = 1;
+				}
 			}
 		}
+		if (choice == 3)
+		{
+			transactionMenu = true; 
+			while (transactionMenu)
+			{
+				TransactionLog();
+			}
+		}
+	}
+}
+
+void TransactionLog()
+{
+	system("cls");
+	for (int k = 0; k <= lastTrans; k++)
+	{
+		if (counter == 1)
+		{
+			cout << usrChecker << MM[0] << "$" << transaction[0] << endl;
+			if (counter == 2)
+			{
+				cout << usrChecker << MM[1] << "$" << transaction[1] << endl;
+				if (counter == 3)
+				{
+					cout << usrChecker << MM[2] << "$" << transaction[2] << endl;
+					if (counter == 4)
+					{
+						cout << usrChecker << MM[3] << "$" << transaction[3] << endl;
+						if (counter == 5)
+						{
+							cout << usrChecker << MM[4] << "$" << transaction[4] << endl;
+						}
+					}
+				}
+			}
+		}
+	}
+	cout << "\n1. Back\n";
+	cin >> choice;
+	if (choice == 1)
+	{
+		transactionMenu = false;
+	}
+	if (choice >= 2)
+	{
+		system("cls");
+		cout << "Not valued input\n";
+		system("pause");
+	}
+}
+
+void PinValidator()
+{
+	system("cls");
+	cout << "Pin: ";
+	cin >> pin;
+	if (pin != pinChecker)
+	{
+		cout << "\nIncorrected Pin\n";
+		system("pause");
+	}
+	if (pin == pinChecker)
+	{
+		pinLock = false;
 	}
 }
 
@@ -159,18 +310,22 @@ void Pincode()
 		cout << "Create a Pin for account: ";
 		cin >> pin;
 
-		if (pin < 1000 || pin > 9999) {
+		if (pin < 1000 || pin > 9999) 
+		{
 			pinNum = false;
 			break;
 		}
-		else {
+		else
+		{
 			break;
 		}
 
 
 	}
-	if (!pinNum) {
+	if (!pinNum) 
+	{
 		Pincode();
 		return;
 	}
+	pinChecker = pin;
 }
